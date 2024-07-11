e92dbf3391a8ef1d2a0982448b6ec98f7e2fd96c
wesleykam <wesley_kam@ucsb.edu>
2024-07-10 17:25:21 -0700

    add hardcoded shapes over calendar

--- src/Calendar.js
+++ src/Calendar.js
@@ -10,33 +10,52 @@ const CALENDAR_DATA = [
     [29, 30, 31]
 ]
 
-// const BLANK_GRID = [
-//     [0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0]
-// ]
+const HARDCODED_SHAPE_GRID = [
+    [1, 2, 2, 2, 3, 3],
+    [1, 1, 6, 2, 0, 3],
+    [1, 1, 6, 2, 3, 3, 4],
+    [7, 7, 6, 5, 5, 4, 4],
+    [7, 7, 6, 6, 5, 0, 4],
+    [7, 7, 8, 8, 5, 5, 4],
+    [8, 8, 8]
+]
 
 function Calendar() {
     return (
-        <div className='calendar-container'>
-            {
-                CALENDAR_DATA.map((row, rowIndex) => {
-                    return <div key={'row-' + rowIndex} className='row'>
-                        {
-                            row.map((cellValue, colIndex) => {
-                                return <div key={colIndex} className='cell'>
-                                    {cellValue}
-                                </div>
-                            })
-                        }
-                    </div>
-                })
-            }
-        </div>
+        <>
+            <div className='calendar-container'>
+                <div className='calendar'>
+                    {
+                        CALENDAR_DATA.map((row, rowIndex) => {
+                            return <div key={'calendar-row-' + rowIndex} className='row'>
+                                {
+                                    row.map((cellValue, colIndex) => {
+                                        return <div key={'calendar-col-' + colIndex} className='cell calendar-cell'>
+                                            {cellValue}
+                                        </div>
+                                    })
+                                }
+                            </div>
+                        })
+                    }
+                </div>
+            </div>
+            <div className='shape-grid-container'>
+                <div className='shape-grid'>
+                    {
+                        HARDCODED_SHAPE_GRID.map((row, rowIndex) => {
+                            return <div key={'shapes-row-' + rowIndex} className='row'>
+                                {
+                                    row.map((cell, colIndex) => {
+                                        return <div key={'shapes-col-' + colIndex} className={'cell shape-' + cell} />
+                                    })
+                                }
+                            </div>
+                        })
+                    }
+                </div>
+            </div>
+        </>
     )
 }
 
